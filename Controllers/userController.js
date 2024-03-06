4fada9e12c7e0d89088d77548a082d89751114ce
Gorla Dinesh <dineshg@trinovatek.com>
2024-03-06 17:54:07 +0530

    nothing

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -79,28 +79,15 @@ const getTokenUsingRefreshToken = async (req, res, next) => {
   }
 };
 
-// const getAllUsers = async (req, res, next) => {
-//   try {
-//     //validate token and then return all users except the current user
-//     const token = req.headers.authorization.split(" ")[1];
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     if (!decoded) {
-//       return res.status(401).send("Invalid token");
-//     }
-//     const users = await User.find({ _id: { $ne: decoded.id } });
-//     // don't send password to client
-//     users.forEach((user) => {
-//       delete user.password;
-//     });
-//     res.status(200).json(users);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
 const getAllUsers = async (req, res, next) => {
   try {
-    const users = await User.find();
+    //validate token and then return all users except the current user
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).send("Invalid token");
+    }
+    const users = await User.find({ _id: { $ne: decoded.id } });
     // don't send password to client
     users.forEach((user) => {
       delete user.password;
