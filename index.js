8fd9ac06975435f5f494566804a598cb69256889
Gorla Dinesh <dineshg@trinovatek.com>
2024-03-07 11:05:10 +0530

    new changes

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const mongoose = require("mongoose");
 const loginRouter = require("./Routes/loginRoutes");
 const usersRouter = require("./Routes/userRoutes");
 const messageRouter = require("./Routes/messageRoute");
-// const { Server } = require("socket.io");
 const Message = require("./Models/messageModels");
+
 // dotenv
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT ||  5000;
 const uri = process.env.MONGO_URL;
 
 app.use(express.json());
@@ -24,44 +24,22 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-const connectDB = async () => {
+mongoose.set("strictQuery", false);
+const dbConnection = async () => {
   try {
-    const conn = await mongoose.connect(`${uri}`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(uri);
+    console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
-    process.exit(1);
   }
-}
+};
 
-
-connectDB().then(() => {
+dbConnection().then(() => {
   app.listen(port, () => {
-      console.log("listening for requests");
+    console.log(`Server running at http://localhost:${port}`);
   });
 }).catch((err) => {
   console.log(err);
 });
 
-// const server = app.listen(port, () => {
-//   console.log(`Server running at http://localhost:${port}`);
-// });
 
-// const io = new Server(server, {
-//   pingTimeout: 60000,
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//   },
-// });
-
-// io.on("connection", (socket) => {
-//   console.log(`User Connected: ${socket.id}`);
-//   // Disconnect
-//   socket.on("disconnect", () => {
-//     console.log("User Disconnected", socket.id);
-//   });
-// });
